03bd30ee18b810a2c4bd05f71ea832683f9b06f5
jonmldr <jonmldr@users.noreply.github.com>
2020-09-26 13:11:41 +0200

    doc block improvement NumberInterface

--- src/NumberInterface.php
+++ src/NumberInterface.php
@@ -8,6 +8,12 @@ use Madebybob\Number\Formatter\FormatterInterface;
 
 interface NumberInterface
 {
+    /**
+     * Returns a new instance of the class which extends AbstractNumber::class.
+     *
+     * @param AbstractNumber|string|float|int $value
+     */
     public function init($value): AbstractNumber;
+
     public function formatter(): FormatterInterface;
 }
